6349ba51847c10e538f58824af66c849c1e04f1b
NIKITA <googelmoogle@gmial.com>
2022-11-07 01:49:53 +0300

    не укладываюсь по времени

--- Kata.cs
+++ Kata.cs
@@ -1,49 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Linq;
 
 namespace CW221104_3_fibonachi
 {
-    public class fibocollection
+
+    static class extends
     {
-        int limit;
-        static SortedSet<long> a = new SortedSet<long>();
-        public fibocollection(int number)
-        {
-            limit = number;
-            if (limit < 1)
-                throw new ArgumentOutOfRangeException("ограничитель числа фибоначи не может быть 0 или отрицательны числом.");
-            if (a.Count == 0)
-            {
-                a.Add(0);
-                a.Add(1);
-            }
-            while (add_f())
-            { }
+        static public void Add(this Array obj, int k)
+        {            
+            psefibo.a[psefibo.count]= k;
+            psefibo.count++;
         }
-        
-        bool add_f()
+    }
+
+    public class psefibo 
+    {
+        public static int[] a;
+        public static int count;
+        static psefibo()
         {
-            long n = a.TakeLast(1).Single() + a.SkipLast(1).TakeLast(1).Single();
-            if (n < limit)
-            {
-                a.Add(n);
-                return true;
-            }
-            else
-                return false;
+            psefibo.a = new int[1000000009];
+            a.Add(1);
+            a.Add(1);
+            a.Add(2);
+
+            var t0 = DateTime.Now;
+            psefibo.prepare(100000000);
+            var t1 = DateTime.Now;
+            psefibo.prepare(200000000);
+            var t2 = DateTime.Now;
+            psefibo.prepare(1000000009);
+            var t3 = DateTime.Now;
+            Console.WriteLine(
+$@"100000000:   {t1.Subtract(t0)}  
+200000000:   {t2.Subtract(t1)}
+1000000009:  {t3.Subtract(t0)}");
+
         }
 
-        static long get_before(long n)
-        { a. }
+        static void prepare(int count)
+        {
+            int c;
+            int o = 3;
+            while(psefibo.count < count)
+            {
+                psefibo.a[psefibo.count] = psefibo.a[psefibo.count - 1] + psefibo.a[psefibo.count - 2];
+                if (o > 20)
+                {
+                    psefibo.a[psefibo.count] %= 10; /// = c % 10;
+                    psefibo.a[psefibo.count - 1] %= 10;
+                    o = 0;
+                }
+                psefibo.count++;
+                o++;
+            }
+            
+        }
+
+        public psefibo(int count)
+        {
+            int c;
+            while (psefibo.count < count)
+            {
+                c = a[psefibo.count - 1] + a[psefibo.count - 2];
+                if (c > 9)
+                    a.Add(c % 10);
+                else
+                    a.Add(c);
+            }
+        }
+        public int LastFibDigit(int n) => psefibo.a[n - 1];
     }
     public class Kata
     {
-        public static int LastFibDigit(long n)
+        public static int LastFibDigit(int n)
         {
-            var b = new fibocollection(6);
-            return 0;
+            var re =  (new psefibo(n)).LastFibDigit(n);
+            if (re > 9) re %= 10;
+            return re; 
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace CW221104_3_fibonachi
     {
         static void Main(string[] args)
         {
-            Kata.LastFibDigit(100000);
+            //Kata.LastFibDigit(10000000000);
+            Console.WriteLine($@"{Kata.LastFibDigit(1) }");
+            Console.WriteLine($@"{ Kata.LastFibDigit(21)}");
+            Console.WriteLine($@"{ Kata.LastFibDigit(302)}");
+            Console.WriteLine($@"{ Kata.LastFibDigit(4003)}");
+            Console.WriteLine($@"{ Kata.LastFibDigit(50004)}");
+            Console.WriteLine($@"{ Kata.LastFibDigit(600005)}");
+            Console.WriteLine($@"{ Kata.LastFibDigit(7000006)}");
         }
     }
 }
